3c4605e17d263a51d963678fe5d42af87bccc707
Julio Arruda <julio.arruda@outlook.com>
2023-05-19 16:20:20 +0000

    including app

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace poc_copilot.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public string Username { get; set; }
+
+        [BindProperty]
+        public string Password { get; set; }
+
+        public UpdateResult UpdateResult { get; set; }
+
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+            if (Username == "admin" && Password == "admin")
+            {
+                UpdateResult = new UpdateResult
+                {
+                    Type = "success",
+                    Message = "Login successful!"
+                };
+            }
+            else
+            {
+                UpdateResult = new UpdateResult
+                {
+                    Type = "danger",
+                    Message = "Invalid credentials!"
+                };
+            }
+        }
+    }
+
+    public class UpdateResult
+    {
+        public string Type { get; set; }
+        public string Message { get; set; }
+    }
+}
